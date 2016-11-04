59f642513a280d564c2540848f212cd3fc3e2851
lilinj2000 <lilinj2000@gmail.com>
2016-11-04 10:02:21 +0000

    the env is ready now.

--- cp.sh
+++ cp.sh
@@ -6,3 +6,11 @@ cp ~/app/cath_0_5/bin/*.data $data_dir
 
 cp ~/app/zeta_zeusing_12345/bin/*.data $data_dir
 
+cp ~/app/zeta_zeusing_12335/bin/*.data $data_dir
+
+cp ~/app/zeta_zeusing_12325/bin/*.data $data_dir
+
+cp ~/app/zeta_zeusing_22235/bin/*.data $data_dir
+
+cp ~/app/zeta_xele_28929/bin/*.data $data_dir
+
--- start_all.sh
+++ start_all.sh
@@ -5,9 +5,28 @@ cd ~/app/cath_0_5/bin
 source ./env.sh
 ./cath >/dev/null 2>&1 &
 
-
 ####  zeusing 12345 ###################
 cd ~/app/zeta_zeusing_12345/bin
+source ./env.sh
+~/cedar/sudo ./zeta >/dev/null 2>&1 &
+
+####  zeusing 12335 ###################
+cd ~/app/zeta_zeusing_12335/bin
+source ./env.sh
+~/cedar/sudo ./zeta >/dev/null 2>&1 &
+
+####  zeusing 12325 ###################
+cd ~/app/zeta_zeusing_12325/bin
+source ./env.sh
+~/cedar/sudo ./zeta >/dev/null 2>&1 &
+
+####  zeusing 22235 ###################
+cd ~/app/zeta_zeusing_22235/bin
+source ./env.sh
+~/cedar/sudo ./zeta >/dev/null 2>&1 &
+
+####  xele 28929 ###################
+cd ~/app/zeta_xele_28929/bin
 source ./env.sh
 ~/cedar/sudo ./zeta >/dev/null 2>&1 &
 
